8b3475ec7ef7a61169c668fb6a17d735301b6f88
Federico Blengio <fedeeblengio@gmail.com>
2021-10-23 06:56:35 -0700

    Task-0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: given array to be sorted
+ * @size: array's size
+ */
+void bubble_sort(int *array, size_t size)
+{
+        int aux;
+	size_t f, j;
+
+	for (f = 0; f < size; f++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+			}
+		}
+	}
+}
