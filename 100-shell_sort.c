807722c69341ddcb30d7042ea49ae15c467e7f78
Federico Blengio <fedeeblengio@gmail.com>
2021-10-28 13:37:43 -0700

    100

---
+++ 100-shell_sort.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+/**
+ *intercambiador_shell - blblblbl
+ *@array: random number
+ *@l: integer int
+ *@size: array size
+ *@intervalo: interval
+ *Return: cero
+ */
+void shell_sort(int *array, size_t size){
+
+	if (array || size)
+	{
+		return;
+	}
+}
