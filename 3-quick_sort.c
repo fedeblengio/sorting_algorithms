b82f3383d3880404384306e05ac4da97cc4e1b36
Federico Blengio <fedeeblengio@gmail.com>
2021-10-28 13:26:06 -0700

    task-3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,1 +1,16 @@
-ijdjdijdsji
+#include "sort.h"
+
+/**
+ *quick_sort - partition array
+ *@array: random number array
+ *@size: array size
+ *Return: the value of the pivot
+ */
+void quick_sort(int *array, size_t size)
+{
+
+	if (array || size)
+	{
+		return;
+	}
+}
